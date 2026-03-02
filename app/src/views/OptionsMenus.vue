9a90117932100d0d55e954b550cf656d5b1c2898
WWeimingW <158204018+WWeimingW@users.noreply.github.com>
2026-03-02 13:05:14 -0500

    asdasd

--- app/src/views/OptionsMenus.vue
+++ app/src/views/OptionsMenus.vue
@@ -6,26 +6,27 @@
       <h3>{{ item.name }}</h3>
       <p>Category: {{ item.category }}</p>
       <p>Price: ${{ item.price.toFixed(2) }}</p>
-      <button @click="store.addToCart(item)">Add to Cart</button>
+      <button @click="addtocart(item)">Add to Cart</button>
     </div>
 
     <h2>Cart</h2>
-    <div v-if="store.cart.length === 0">Cart is empty</div>
-
-    <div v-else>
-      <div v-for="item in store.cart" :key="item.id">
-        {{ item.name }} x {{ item.quantity }} - ${{ (item.price * item.quantity).toFixed(2) }}
-        <button @click="store.removeFromCart(item.id)">Remove</button>
-      </div>
-
-      <h3>Total: ${{ store.total.toFixed(2) }}</h3>
+    <div v-if="cart.length === 0">
+      <p>Your cart is empty.</p>
     </div>
+    <div v-for="item in cart" :key="item.id" class="cart-item">
+      <h4>{{ item.name }}</h4>
+      <p>${{ item.price.toFixed(2) }} x {{ item.quantity }}</p>
+      <button @click="item.quantity++">+</button>
+      <button @click="decreasequantity(item)">-</button>
+      <button @click="removefromcart(item.id)">Remove</button>
+    </div>
+
+    <h3 v-if="cart.length > 0">Total: ${{ totalPrice.toFixed(2) }}</h3>
   </div>
 </template>
 
 <script setup>
 import { ref } from 'vue'
-import { store } from '../stores/store.js'
 
 const menuItems = ref([
   { id: 1, name: 'Big Mac', price: 5.99, category: 'Burger' },
@@ -33,6 +34,21 @@ const menuItems = ref([
   { id: 3, name: 'Fries', price: 2.49, category: 'Sides' },
   { id: 4, name: 'McFlurry', price: 3.99, category: 'Dessert' },
 ])
+
+const cart = ref([])
+
+const addtocart = (item) => {
+  const Item = cart.value.find((i) => i.id === item.id)
+
+  if (Item) {
+    Item.quantity++
+  } else {
+    cart.value.push({ ...item, quantity: 1 })
+  }
+}
+const decreasequantity
+
+const removefromcart
 </script>
 
 <style>
@@ -47,4 +63,3 @@ button {
   margin-right: 5px;
 }
 </style>
-<!-- absolute ref top left 50 50 how to place items on top -->
--- app/src/views/stores/store.js
+++ app/src/views/stores/store.js
@@ -1,14 +1,1 @@
-import { reactive } from 'vue'
 
-export const store = reactive({
-  cart: [],
-
-  addToCart(item) {
-    const existingItem = this.cart.find((i) => i.id === item.id)
-    if (existingItem) {
-      existingItem.quantity++
-    } else {
-      this.cart.push({ ...item, quantity: 1 })
-    }
-  },
-})
