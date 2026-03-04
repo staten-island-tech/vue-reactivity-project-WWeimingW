5028ab63b24a5458ab4597a127f6832aeadfcbfb
WWeimingW <158204018+WWeimingW@users.noreply.github.com>
2026-03-04 13:51:32 -0500

    dhxgfh

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import OptionsMenus from '../../components/OptionsMenus.vue'
+import OptionsMenus from '../views/OptionsMenus.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
